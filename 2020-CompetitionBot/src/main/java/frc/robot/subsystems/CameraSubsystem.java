a8dbd8c6deac2f26ebd97b4c232abddb0d40e79f
Ryan-Afzal <ryan.afzal@outlook.com>
2020-02-22 21:10:14 -0800

    Update CameraSubsystem.java

--- 2020-CompetitionBot/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ 2020-CompetitionBot/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -6,15 +6,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CameraSubsystem extends SubsystemBase {
 
-	private UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+	private final UsbCamera driverCamera = CameraServer.getInstance().startAutomaticCapture(0);
+	private final UsbCamera operatorCamera = CameraServer.getInstance().startAutomaticCapture(1);
 
 	public CameraSubsystem() {
-		this.camera.setResolution(160,90);
-		this.camera.setFPS(25);
+		this.driverCamera.setResolution(160,90);
+		this.driverCamera.setFPS(30);
+
+		this.operatorCamera.setResolution(160, 90);
+		this.operatorCamera.setFPS(25);
 	}
 
-	public UsbCamera getCamera() {
-		return this.camera;
+	public UsbCamera getDriverCamera() {
+		return this.driverCamera;
+	}
+
+	public UsbCamera getOperatoCamera() {
+		return this.operatorCamera;
 	}
 
 }
